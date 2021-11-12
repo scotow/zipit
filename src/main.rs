ff65ed554b290d4a290e2d2398d642e3710b7e8c
Scotow <contact@scotow.com>
2021-11-13 00:13:26 +0100

    Refactor using macro

--- src/main.rs
+++ src/main.rs
@@ -102,23 +102,20 @@ impl<W: AsyncWrite + Unpin> Archive<W> {
     pub async fn append<R: AsyncRead + Unpin>(&mut self, name: String, datetime: FileDateTime, reader: &mut R) -> Result<(), TokioIoError> {
         let (date, time) = datetime.ms_dos();
         let offset = self.written;
-        // let mut header = header![
-        //     7 * size_of::<u16>() + 4 * size_of::<u32>() + name.len();
-        //     0x04034b50u32,
-        // ];
-        let mut header = Vec::with_capacity(7 * size_of::<u16>() + 4 * size_of::<u32>() + name.len());
-
-        header.extend_from_slice(&0x04034b50u32.to_le_bytes()); // Local file header signature.
-        header.extend_from_slice(&10u16.to_le_bytes()); // Version needed to extract.
-        header.extend_from_slice(&0x08u16.to_le_bytes()); // General purpose flag.
-        header.extend_from_slice(&0u16.to_le_bytes()); // Compression method (store).
-        header.extend_from_slice(&time.to_le_bytes()); // Modification time.
-        header.extend_from_slice(&date.to_le_bytes()); // Modification date.
-        header.extend_from_slice(&0u32.to_le_bytes()); // Temporary CRC32.
-        header.extend_from_slice(&0u32.to_le_bytes()); // Temporary compressed size.
-        header.extend_from_slice(&0u32.to_le_bytes()); // Temporary uncompressed size.
-        header.extend_from_slice(&(name.len() as u16).to_le_bytes()); // Filename length.
-        header.extend_from_slice(&0u16.to_le_bytes()); // Extra field length.
+        let mut header = header![
+            7 * size_of::<u16>() + 4 * size_of::<u32>() + name.len();
+            0x04034b50u32,          // Local file header signature.
+            10u16,                  // Version needed to extract.
+            0x08u16,                // General purpose flag.
+            0u16,                   // Compression method (store).
+            time,                   // Modification time.
+            date,                   // Modification date.
+            0u32,                   // Temporary CRC32.
+            0u32,                   // Temporary compressed size.
+            0u32,                   // Temporary uncompressed size.
+            name.len() as u16,      // Filename length.
+            0u16,                   // Extra field length.
+        ];
         header.extend_from_slice(name.as_bytes()); // Filename.
         self.sink.write_all(&header).await?;
         self.written += header.len();
@@ -139,11 +136,13 @@ impl<W: AsyncWrite + Unpin> Archive<W> {
         let crc = hasher.finalize();
         self.written += total_read;
 
-        let mut descriptor = Vec::with_capacity(4 * size_of::<u32>());
-        descriptor.extend_from_slice(&0x08074b50u32.to_le_bytes()); // Data descriptor signature.
-        descriptor.extend_from_slice(&crc.to_le_bytes()); // CRC32.
-        descriptor.extend_from_slice(&(total_read as u32).to_le_bytes()); // Compressed size.
-        descriptor.extend_from_slice(&(total_read as u32).to_le_bytes()); // Uncompressed size.
+        let descriptor = header![
+            4 * size_of::<u32>();
+            0x08074b50u32,      // Data descriptor signature.
+            crc,                // CRC32.
+            total_read as u32,  // Compressed size.
+            total_read as u32,  // Uncompressed size.
+        ];
         self.sink.write_all(&descriptor).await?;
         self.written += descriptor.len();
 
@@ -161,45 +160,42 @@ impl<W: AsyncWrite + Unpin> Archive<W> {
     pub async fn finalize(mut self) -> Result<W, TokioIoError> {
         let mut central_directory_size = 0;
         for file_info in &self.files_info {
-            let entry_size = 11 * size_of::<u16>() +
-                6 * size_of::<u32>() +
-                file_info.name.len();
-            central_directory_size += entry_size;
-
-            let mut entry = Vec::with_capacity(entry_size);
-            entry.extend_from_slice(&0x02014b50u32.to_le_bytes()); // Central directory entry signature.
-            entry.extend_from_slice(&0x031eu16.to_le_bytes()); // Version made by.
-            entry.extend_from_slice(&10u16.to_le_bytes()); // Version needed to extract.
-            entry.extend_from_slice(&0x08u16.to_le_bytes()); // General purpose flag.
-            entry.extend_from_slice(&0u16.to_le_bytes()); // Compression method (store).
-            entry.extend_from_slice(&file_info.datetime.1.to_le_bytes()); // Modification time.
-            entry.extend_from_slice(&file_info.datetime.0.to_le_bytes()); // Modification date.
-            entry.extend_from_slice(&file_info.crc.to_le_bytes()); // CRC32.
-            entry.extend_from_slice(&(file_info.size as u32).to_le_bytes()); // Compressed size.
-            entry.extend_from_slice(&(file_info.size as u32).to_le_bytes()); // Uncompressed size.
-            entry.extend_from_slice(&(file_info.name.len() as u16).to_le_bytes()); // Filename length.
-            entry.extend_from_slice(&0u16.to_le_bytes()); // Extra field length.
-            entry.extend_from_slice(&0u16.to_le_bytes()); // File comment length.
-            entry.extend_from_slice(&0u16.to_le_bytes()); // File's Disk number.
-            entry.extend_from_slice(&0u16.to_le_bytes()); // Internal file attributes.
-            entry.extend_from_slice(&((0o100000u32 | 0o0000400 | 0o0000200 | 0o0000040 | 0o0000004) << 16).to_le_bytes()); // External file attributes (regular file rw-r-r-).
-            entry.extend_from_slice(&(file_info.offset as u32).to_le_bytes()); // Offset from start of file to local file header.
+            let mut entry = header![
+                11 * size_of::<u16>() + 6 * size_of::<u32>() + file_info.name.len();
+                0x02014b50u32,                  // Central directory entry signature.
+                0x031eu16,                      // Version made by.
+                10u16,                          // Version needed to extract.
+                0x08u16,                        // General purpose flag.
+                0u16,                           // Compression method (store).
+                file_info.datetime.1,           // Modification time.
+                file_info.datetime.0,           // Modification date.
+                file_info.crc,                  // CRC32.
+                file_info.size as u32,          // Compressed size.
+                file_info.size as u32,          // Uncompressed size.
+                file_info.name.len() as u16,    // Filename length.
+                0u16,                           // Extra field length.
+                0u16,                           // File comment length.
+                0u16,                           // File's Disk number.
+                0u16,                           // Internal file attributes.
+                (0o100000u32 | 0o0000400 | 0o0000200 | 0o0000040 | 0o0000004) << 16, // External file attributes (regular file rw-r-r-).
+                file_info.offset as u32,        // Offset from start of file to local file header.
+            ];
             entry.extend_from_slice(file_info.name.as_bytes()); // Filename.
             self.sink.write_all(&entry).await?;
+            central_directory_size += entry.len();
         }
 
-        let mut end_of_central_directory = Vec::with_capacity(
-            5 * size_of::<u16>() +
-                3 * size_of::<u32>()
-        );
-        end_of_central_directory.extend_from_slice(&0x06054b50u32.to_le_bytes()); // End of central directory signature.
-        end_of_central_directory.extend_from_slice(&0u16.to_le_bytes()); // Number of this disk.
-        end_of_central_directory.extend_from_slice(&0u16.to_le_bytes()); // Number of the disk where central directory starts.
-        end_of_central_directory.extend_from_slice(&(self.files_info.len() as u16).to_le_bytes()); // Number of central directory records on this disk.
-        end_of_central_directory.extend_from_slice(&(self.files_info.len() as u16).to_le_bytes()); // Total number of central directory records.
-        end_of_central_directory.extend_from_slice(&(central_directory_size as u32).to_le_bytes()); // Size of central directory.
-        end_of_central_directory.extend_from_slice(&(self.written as u32).to_le_bytes()); // Offset from start of file to central directory.
-        end_of_central_directory.extend_from_slice(&0u16.to_le_bytes()); // Comment length.
+        let end_of_central_directory =header![
+            5 * size_of::<u16>() + 3 * size_of::<u32>();
+            0x06054b50u32,                  // End of central directory signature.
+            0u16,                           // Number of this disk.
+            0u16,                           // Number of the disk where central directory starts.
+            self.files_info.len() as u16,   // Number of central directory records on this disk.
+            self.files_info.len() as u16,   // Total number of central directory records.
+            central_directory_size as u32,  // Size of central directory.
+            self.written as u32,            // Offset from start of file to central directory.
+            0u16,                           // Comment length.
+        ];
         self.sink.write_all(&end_of_central_directory).await?;
 
         Ok(self.sink)
