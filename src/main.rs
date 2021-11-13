4885b0222dee67717f6e5a096c6945faf9b417e2
Scotow <contact@scotow.com>
2021-11-13 13:31:59 +0100

    Add archive structure test

--- src/main.rs
+++ src/main.rs
@@ -201,3 +201,42 @@ impl<W: AsyncWrite + Unpin> Archive<W> {
         Ok(self.sink)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+    use crate::{Archive, FileDateTime};
+
+    #[tokio::test]
+    async fn archive_structure() {
+        let mut archive = Archive::new(Vec::new());
+        archive.append(
+            "file1.txt".to_owned(),
+            FileDateTime::now(),
+            &mut Cursor::new(b"hello\n".to_vec()),
+        ).await.unwrap();
+        archive.append(
+            "file2.txt".to_owned(),
+            FileDateTime::now(),
+            &mut Cursor::new(b"world\n".to_vec()),
+        ).await.unwrap();
+        let data = archive.finalize().await.unwrap();
+
+        fn match_except_datetime(a1: &[u8], a2: &[u8]) -> bool {
+            let datetime_ranges = [10..12, 12..14, 71..73, 73..75, 134..136, 136..138, 189..191, 191..193];
+            let size_ranges = [18..22, 22..26, 79..83, 83..87];
+            a1.len() == a2.len() &&
+                a1.into_iter()
+                    .zip(a2)
+                    .enumerate()
+                    .filter(|(i, _)| {
+                        datetime_ranges.iter()
+                            .chain(&size_ranges)
+                            .all(|range| !range.contains(i))
+                    })
+                    .all(|(_, (b1, b2))| b1 == b2)
+        }
+        assert!(match_except_datetime(&data, include_bytes!("timeless_test_archive.zip")));
+        assert!(match_except_datetime(&data, include_bytes!("zip_command_test_archive.zip")));
+    }
+}
